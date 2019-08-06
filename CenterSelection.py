7b695bdd76d1ad2494aa2ee9308aed05aebfac89
leochuanxing <liuchuansheng@gmail.com>
2019-08-07 01:15:41 +0800

    IOF matrix

--- CenterSelection.py
+++ CenterSelection.py
@@ -11,7 +11,7 @@ os.chdir('/home/leo/Documents/Project_SelectCenters/DATA/BreastCancer')
 data = pd.read_csv('breast-cancer-wisconsin.data')
 data.dtypes
 col_name = data.columns
-data[data[col_name[2]] == '?']
+
 missing = data[:][data[col_name[6]] == '?']
 missing.shape
 complete = data[:][data[col_name[6]] != '?']
@@ -25,9 +25,27 @@ nrow, ncol = df_att.shape
 distance_matrix = np.zeros((nrow, nrow))
 df_att.head()
 complete.head()
+col = df_att[attributes[0]]
 
 
-distance_matrix = distance_matrix/ncol
+
+def Frequency_distinct(col):
+    distinct_values = list(set(col))
+    df_element = pd.DataFrame({'d_values':[], 'freq':[]})
+    for i in distinct_values:
+        df_element = df_element.append({'d_values':i, 'freq':sum(col == i)},\
+                                        ignore_index = True)
+    
+    freq_col = col.apply(lambda x:np.int(df_element[x==df_element['d_values']]['freq']))
+        
+    return freq_col
+
+freq_df = df_att.apply(Frequency_distinct, axis = 0)
+np.log(freq_df)
+df_att.iloc[1].equals(df_att.iloc[3])
+df_att.iloc[1]
+df_att.iloc[3]
+
 
 '''
 Preparation is for data wrangling and calculate the distance matrix according to the
@@ -46,8 +64,22 @@ class Distance_martix:
                 distance_matrix[j,i] = distance_matrix[i,j]
         self.hamming_matrix = distance_matrix/ncol
         
-    def IOF_matrix():
-        pass
+    def IOF_matrix(self):
+        freq_frame = self.att.apply(Frequency_distinct, axis = 0)
+        freq_array_log = np.log(freq_frame).values
+        self.freq_frame = freq_frame
+        
+        nrow, ncol = self.att.shape
+        distance_matrix = np.zeros((nrow, nrow))
+        for i in range(nrow):
+            for j in range(i, nrow):
+                if df_att.iloc[i].equals(df_att.iloc[j]):
+                    distance_matrix[i,j] = 0
+                else:
+                    distance_matrix[i,j] = np.sum(freq_array_log[i]*freq_array_log[j])
+                distance_matrix[j,i] = distance_matrix[i,j]
+        self.iof_matrix = distance_matrix/ncol
+
     def OF_matrix():
         pass
     def Lin_matrix():
